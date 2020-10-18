68e700e6c8e7f766c68fef1da9eacc4b8a171402
Beatriz Martínez <beatriz@isovalent.com>
2020-10-18 11:11:20 +0200

    uprobes example added

--- app-x/app-3/main.go
+++ app-x/app-3/main.go
@@ -1,15 +1,15 @@
 package main
 
 import (
-    "fmt"
-    "net/http"
+	"fmt"
+	"net/http"
 )
 
 func main() {
-    http.HandleFunc("/", HelloServer)
-    http.ListenAndServe(":8083", nil)
+	http.HandleFunc("/", HelloServer)
+	http.ListenAndServe(":8083", nil)
 }
 
 func HelloServer(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "Submitted word: %s!\n", r.URL.Path[1:])
+	fmt.Fprintf(w, "Submitted word: %s!\n", r.URL.Path[1:])
 }
